8f8c8130e814d8dbf272fcff9cb4caad4f74c523
gabrielsmm <gabrielsmm251@gmail.com>
2023-07-11 18:42:05 -0300

    feat: Estruturação das entidades e repositorios

---
+++ src/main/java/br/com/banco/entities/Conta.java
@@ -0,0 +1,28 @@
+package br.com.banco.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "CONTA")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Conta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nomeResponsavel;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "conta")
+    private List<Transferencia> transferencias = new ArrayList<>();
+
+}
---
+++ src/main/java/br/com/banco/entities/Transferencia.java
@@ -0,0 +1,29 @@
+package br.com.banco.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "TRANSFERENCIA")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Transferencia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date dataTransferencia;
+    private Double valor;
+    private String tipo;
+    private String nomeOperadorTransacao;
+
+    @ManyToOne
+    @JoinColumn(name = "conta_id")
+    private Conta conta;
+
+}
